bd3d634fb291e5d6be316a9dc6ee0bd67e5c60ed
Martin Main <martin@fundersclub.com>
2024-11-15 22:45:05 -0400

    Updated structure

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ from setuptools import (
 
 setup(
     name='business_days',
-    version='0.0.5',
-    package_dir={"": "business_days"},
-    packages=find_packages(where="business_days"),
+    version='0.0.6',
+    packages=find_packages(),
     test_suite='nose.collector',
     tests_require=['nose'],
     license='Apache License 2.0',
     description='A tiny little library for handling business days with dates',
     url='https://www.github.com/FundersClub/business_days',
     classifiers=[
-        'Programding Language :: Python :: 2',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Apache Software License',
         'Intended Audience :: Developers',
     ],
